db21bf9a7428322dfa2dd95480ad7d9cf0d22e90
Leo Hong <hongliyu2002@hotmail.com>
2022-05-16 23:07:39 +0800

    添加本地化共享项目及模块。

---
+++ shared/EShop.Shared.Localization/EShopSharedLocalizationModule.cs
@@ -0,0 +1,49 @@
+﻿using EShop.Localization.Localization;
+using Volo.Abp.Localization;
+using Volo.Abp.Modularity;
+using Volo.Abp.Validation;
+using Volo.Abp.Validation.Localization;
+using Volo.Abp.VirtualFileSystem;
+
+namespace EShop.Localization;
+
+[DependsOn(typeof(AbpValidationModule))]
+public class EShopSharedLocalizationModule : AbpModule
+{
+
+    #region Overrides of AbpModule
+
+    /// <inheritdoc />
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        ConfigureVirtualFileSystem();
+        ConfigureLocalization();
+    }
+
+    #endregion
+
+    private void ConfigureVirtualFileSystem()
+    {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+                                               {
+                                                   options.FileSets.AddEmbedded<EShopSharedLocalizationModule>();
+                                               });
+    }
+
+    /// <summary>
+    ///     Adding the supported languages.
+    /// </summary>
+    /// <see>
+    ///     <cref>https://docs.abp.io/en/abp/latest/Localization</cref>
+    /// </see>
+    private void ConfigureLocalization()
+    {
+        Configure<AbpLocalizationOptions>(options =>
+                                          {
+                                              options.Languages.Add(new LanguageInfo("en", "en", "English"));
+                                              options.Languages.Add(new LanguageInfo("zh-Hans", "zh-Hans", "简体中文"));
+                                              options.Resources.Add<EShopResource>("en").AddBaseTypes(typeof(AbpValidationResource)).AddVirtualJson("/Localization/Resources");
+                                              options.DefaultResourceType = typeof(EShopResource);
+                                          });
+    }
+}
---
+++ shared/EShop.Shared.Localization/Localization/EShopResource.cs
@@ -0,0 +1,8 @@
+﻿using Volo.Abp.Localization;
+
+namespace EShop.Localization.Localization;
+
+[LocalizationResourceName("EShop")]
+public class EShopResource
+{
+}
