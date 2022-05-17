3c46edb6f7ec46fbd38820a83558c0265270ccaa
Leo Hong <hongliyu2002@hotmail.com>
2022-05-17 23:55:25 +0800

    新建了管理服务解决方案并添加了领域共享项目

---
+++ services/Administration/src/EShop.AdministrationService.Domain.Shared/AdministrationServiceDomainSharedModule.cs
@@ -0,0 +1,47 @@
+﻿using EShop.Localization.Localization;
+using Volo.Abp.AuditLogging;
+using Volo.Abp.Localization;
+using Volo.Abp.Modularity;
+using Volo.Abp.PermissionManagement;
+using Volo.Abp.SettingManagement;
+using Volo.Abp.Validation;
+using Volo.Abp.Validation.Localization;
+using Volo.Abp.VirtualFileSystem;
+
+namespace EShop.AdministrationService;
+
+[DependsOn(typeof(AbpValidationModule))]
+[DependsOn(typeof(AbpAuditLoggingDomainSharedModule))]
+[DependsOn(typeof(AbpPermissionManagementDomainSharedModule))]
+[DependsOn(typeof(AbpSettingManagementDomainSharedModule))]
+public class AdministrationServiceDomainSharedModule : AbpModule
+{
+
+    #region Overrides of AbpModule
+
+    /// <inheritdoc />
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        ConfigureVirtualFileSystem();
+        ConfigureLocalization();
+    }
+
+    #endregion
+
+    private void ConfigureVirtualFileSystem()
+    {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+                                               {
+                                                   options.FileSets.AddEmbedded<AdministrationServiceDomainSharedModule>();
+                                               });
+    }
+
+    private void ConfigureLocalization()
+    {
+        Configure<AbpLocalizationOptions>(options =>
+                                          {
+                                              options.Resources.Add<AdministrationServiceResource>("en").AddBaseTypes(typeof(AbpValidationResource)).AddVirtualJson("/Localization/AdministrationService");
+                                              options.DefaultResourceType = typeof(AdministrationServiceResource);
+                                          });
+    }
+}
