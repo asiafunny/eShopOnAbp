36628b720456b1d4145d26d9ded03fe40826acbb
Leo Hong <hongliyu2002@hotmail.com>
2022-05-17 12:24:11 +0800

    添加主机共享项目AspNetCore扩展及模块

---
+++ shared/EShop.Shared.Hosting.AspNetCore/Helpers/ApplicationBuilderHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Volo.Abp.Modularity;
+
+namespace EShop.Shared.Hosting.AspNetCore.Helpers;
+
+public static class ApplicationBuilderHelper
+{
+    /// <summary>
+    ///     Build a web application.
+    /// </summary>
+    /// <typeparam name="TStartupModule"></typeparam>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static async Task<WebApplication> BuildApplicationAsync<TStartupModule>(string[] args)
+        where TStartupModule : IAbpModule
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Host.AddAppSettingsSecretsJson().UseAutofac().UseSerilog();
+        await builder.AddApplicationAsync<TStartupModule>();
+        return builder.Build();
+    }
+}
---
+++ shared/EShop.Shared.Hosting.AspNetCore/Helpers/SwaggerHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Volo.Abp.Modularity;
+
+namespace EShop.Shared.Hosting.AspNetCore.Helpers;
+
+public static class SwaggerHelper
+{
+    public static void Configure(ServiceConfigurationContext context, string apiTitle, string apiVersion = "v1", string apiName = "v1")
+    {
+        context.Services.AddSwaggerGen(options =>
+                                       {
+                                           options.SwaggerDoc(apiName, new OpenApiInfo { Title = apiTitle, Version = apiVersion });
+                                           options.DocInclusionPredicate((docName, description) => true);
+                                           options.CustomSchemaIds(type => type.FullName);
+                                       });
+    }
+
+    public static void ConfigureWithAuth(ServiceConfigurationContext context, string authority, Dictionary<string, string> scopes, string apiTitle,
+                                         string apiVersion = "v1", string apiName = "v1")
+    {
+        context.Services.AddAbpSwaggerGenWithOAuth(authority,
+                                                   scopes,
+                                                   options =>
+                                                   {
+                                                       options.SwaggerDoc(apiName, new OpenApiInfo { Title = apiTitle, Version = apiVersion });
+                                                       options.DocInclusionPredicate((docName, description) => true);
+                                                       options.CustomSchemaIds(type => type.FullName);
+                                                   });
+    }
+}
