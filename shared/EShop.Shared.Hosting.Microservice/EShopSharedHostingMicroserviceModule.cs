1e8b9d9adfff711d3895d3d8c5bce662ef44f0dd
Leo Hong <hongliyu2002@hotmail.com>
2022-05-17 17:29:33 +0800

    添加主机共享项目微服务模块

---
+++ shared/EShop.Shared.Hosting.Microservice/EShopSharedHostingMicroserviceModule.cs
@@ -0,0 +1,73 @@
+﻿using EShop.Shared.Hosting.AspNetCore;
+using Medallion.Threading;
+using Medallion.Threading.Redis;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+using Volo.Abp.AspNetCore.MultiTenancy;
+using Volo.Abp.BackgroundJobs.RabbitMQ;
+using Volo.Abp.Caching;
+using Volo.Abp.Caching.StackExchangeRedis;
+using Volo.Abp.DistributedLocking;
+using Volo.Abp.EventBus.RabbitMq;
+using Volo.Abp.Modularity;
+using Volo.Abp.MultiTenancy;
+
+namespace EShop.Shared.Hosting.Microservice;
+
+[DependsOn(typeof(AbpAspNetCoreMultiTenancyModule))]
+[DependsOn(typeof(AbpCachingStackExchangeRedisModule))]
+[DependsOn(typeof(AbpDistributedLockingModule))]
+[DependsOn(typeof(AbpEventBusRabbitMqModule))]
+[DependsOn(typeof(AbpBackgroundJobsRabbitMqModule))]
+[DependsOn(typeof(EShopSharedHostingAspNetCoreModule))]
+public class EShopSharedHostingMicroserviceModule : AbpModule
+{
+
+    #region Overrides of AbpModule
+
+    /// <inheritdoc />
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var services = context.Services;
+        var configuration = context.Services.GetConfiguration();
+        ConfigureMultiTenancy();
+        ConfigureDistributedCache();
+        ConfigureDataProtection(services, configuration);
+        ConfigureDistributedLock(services, configuration);
+    }
+
+    #endregion
+
+    private void ConfigureMultiTenancy()
+    {
+        Configure<AbpMultiTenancyOptions>(options =>
+                                          {
+                                              options.IsEnabled = true;
+                                          });
+    }
+
+    private void ConfigureDistributedCache()
+    {
+        Configure<AbpDistributedCacheOptions>(options =>
+                                              {
+                                                  options.KeyPrefix = "eShop:";
+                                              });
+    }
+
+    private void ConfigureDataProtection(IServiceCollection services, IConfiguration configuration)
+    {
+        var redis = ConnectionMultiplexer.Connect(configuration["Redis:Configuration"]);
+        services.AddDataProtection().SetApplicationName("AdministrationService").PersistKeysToStackExchangeRedis(redis, "EShop-Protection-Keys");
+    }
+
+    private void ConfigureDistributedLock(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IDistributedLockProvider>(_ =>
+                                                        {
+                                                            var redis = ConnectionMultiplexer.Connect(configuration["Redis:Configuration"]);
+                                                            return new RedisDistributedSynchronizationProvider(redis.GetDatabase());
+                                                        });
+    }
+}
