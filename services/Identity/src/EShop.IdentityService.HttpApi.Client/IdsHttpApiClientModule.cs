74da81024e84a7d7af7a93cf6a5e170ccbf64a4d
Leo Hong <hongliyu2002@hotmail.com>
2022-05-21 11:17:45 +0800

    添加身份微服务客户端Http重试策略

--- services/Identity/src/EShop.IdentityService.HttpApi.Client/IdsHttpApiClientModule.cs
+++ services/Identity/src/EShop.IdentityService.HttpApi.Client/IdsHttpApiClientModule.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Volo.Abp.Http.Client;
 using Volo.Abp.Identity;
 using Volo.Abp.Modularity;
 
@@ -12,6 +15,12 @@ public class IdsHttpApiClientModule : AbpModule
     #region Overrides of AbpModule
 
     /// <inheritdoc />
+    public override void PreConfigureServices(ServiceConfigurationContext context)
+    {
+        ConfigureHttpClientBuilder();
+    }
+
+    /// <inheritdoc />
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
         var services = context.Services;
@@ -19,6 +28,17 @@ public class IdsHttpApiClientModule : AbpModule
     }
 
     #endregion
+
+    private void ConfigureHttpClientBuilder()
+    {
+        PreConfigure<AbpHttpClientBuilderOptions>(options =>
+                                                  {
+                                                      options.ProxyClientBuildActions.Add((_, builder) =>
+                                                                                          {
+                                                                                              builder.AddTransientHttpErrorPolicy(policyBuilder => policyBuilder.WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(Math.Pow(2, i))));
+                                                                                          });
+                                                  });
+    }
 
     private void ConfigureHttpClientProxies(IServiceCollection services)
     {
